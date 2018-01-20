532ebc1c1f8c4087e58728cc196ec9c76b3d4adf
Shaojung <shaojung@gmail.com>
2018-01-20 14:07:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db2018012004/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.student.db2018012004;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox chk1, chk2;
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        chk1 = findViewById(R.id.checkBox);
+        chk2 = findViewById(R.id.checkBox2);
+        tv = findViewById(R.id.textView);
+    }
+    public void click1(View v)
+    {
+        
+    }
+}
